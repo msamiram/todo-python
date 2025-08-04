f96f6ab42d1cc3bfe0a6ded9919f7005ee2a4cba
msamiram <samirammd01@gmail.com>
2025-08-04 21:01:44 +0400

    Dockerized app, added DB migrations and updated dependencies

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+
+FROM python:3.11-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+ENV FLASK_APP=app.py     
+ENV FLASK_RUN_PORT=5000
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -2,19 +2,26 @@ from flask import Flask
 from extensions import db, jwt
 from config import Config
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 from routes.auth_routes import auth_bp
 from routes.task_routes import task_bp
 from routes.user_routes import user_bp
 from routes.static_routes import static_bp
+from dotenv import load_dotenv
+load_dotenv()
+
+migrate = Migrate()  
 
 def create_app():
     app = Flask(__name__, static_folder=None)
     app.config.from_object(Config)
 
     CORS(app, origins=["http://127.0.0.1:5000", "http://localhost:5000"])
+
     db.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)  
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(task_bp)
@@ -23,17 +30,13 @@ def create_app():
 
     @app.cli.command('init-db')
     def init_db():
-        """Initialize the database."""
-        with app.app_context():
-            db.create_all()
-        print("Initialized the database.")
+        """Run database migrations."""
+        from flask_migrate import upgrade
+        upgrade()
+        print("Database migrated to latest version.")
 
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    with app.app_context():
-        from models.user import User
-        from models.task import Task
-        db.create_all()
     app.run(debug=True)
